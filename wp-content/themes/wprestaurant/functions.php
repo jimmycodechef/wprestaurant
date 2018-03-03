925c3ebcbecaa2ca0bfd20e05dd412de9adbe8e4
jimmy <jimmycodechef@gmail.com>
2018-03-04 02:01:00 +0530

    Start

--- wp-content/themes/wprestaurant/functions.php
+++ wp-content/themes/wprestaurant/functions.php
@@ -2,9 +2,11 @@
 
 //Adding stylesheets to the wordpress
 function lapi_styles(){
-	wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), 6.0);
-	wp_register_style('style', get_template_directory_uri() . '/style.css', array(), 1.0);
+	wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '6.0');
+	wp_register_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '4.7.0');
+	wp_register_style('style', get_template_directory_uri() . '/style.css', array(), '1.0');
 	wp_enqueue_style('normalize');
+	wp_enqueue_style('fontawesome');
 	wp_enqueue_style('style');
 }
 add_action('wp_enqueue_scripts', 'lapi_styles');
