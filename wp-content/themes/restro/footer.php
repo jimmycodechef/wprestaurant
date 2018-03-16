57a37c6752a5dec63cb6da9e99624e17288bbead
Jimmy <jimmycodechef@gmail.com>
2018-03-16 20:50:46 +0530

    Lecture 74

--- wp-content/themes/restro/footer.php
+++ wp-content/themes/restro/footer.php
@@ -1,3 +1,22 @@
+<footer>
+    <?php
+        $args = array(
+                'theme_location' => 'header-menu',
+                'container'      => 'nav',
+                'after'          => '<span class="seperator"> | </span>'
+        );
+        wp_nav_menu($args);
+    ?>
+    <div class="location">
+        <p>2301 Alpha Beta Avenue Mountain Top, KK, TRIP 60231</p>
+        <p>Phone Number: +900-89-785-7800</p>
+    </div>
+
+    <p class="copyright">All rights reserved <?php echo date('Y');?></p>
+</footer>
+
+
+
 
 <?php wp_footer(); ?>
 </body>
--- wp-content/themes/restro/functions.php
+++ wp-content/themes/restro/functions.php
@@ -1,14 +1,24 @@
 <?php
 
+function restro_setup(){
+	add_theme_support('post-thumbnails');
+	add_image_size('boxes', 437, 291, true);
+	add_image_size('specialties', 768, 515, true);
+}
+add_action('after_setup_theme', 'restro_setup');
+
+
 //Adding Stylesheet to theme
 function restro_styles(){
 	//Adding Stylesheets
+	wp_register_style('googlefont', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700|Raleway:400,700,900', array(), '1.0.0');
 	wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.0');
 	wp_register_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.css', array(), '4.7.0');
 	wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
 
 	wp_enqueue_style('normalize');
 	wp_enqueue_style('fontawesome');
+	wp_enqueue_style('googlefont');
 	wp_enqueue_style('style');
 
 	//Registering JavaScript Files
@@ -29,3 +39,44 @@ function restro_menus(){
 
 }
 add_action('init', 'restro_menus');
+
+
+function restro_specialties() {
+	$labels = array(
+		'name'               => _x( 'Pizzas', 'restro' ),
+		'singular_name'      => _x( 'Pizza', 'post type singular name', 'restro' ),
+		'menu_name'          => _x( 'Pizzas', 'admin menu', 'restro' ),
+		'name_admin_bar'     => _x( 'Pizzas', 'add new on admin bar', 'restro' ),
+		'add_new'            => _x( 'Add New', 'book', 'restro' ),
+		'add_new_item'       => __( 'Add New Pizza', 'restro' ),
+		'new_item'           => __( 'New Pizza', 'restro' ),
+		'edit_item'          => __( 'Edit Pizza', 'restro' ),
+		'view_item'          => __( 'View Pizza', 'restro' ),
+		'all_items'          => __( 'All Pizzas', 'restro' ),
+		'search_items'       => __( 'Search Pizzas', 'restro' ),
+		'parent_item_colon'  => __( 'Parent Pizzas:', 'restro' ),
+		'not_found'          => __( 'No Pizzas found.', 'restro' ),
+		'not_found_in_trash' => __( 'No Pizzas found in Trash.', 'restro' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'description'        => __( 'Description.', 'restro' ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'specialties' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 6,
+		'supports'           => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'          => array( 'category' ),
+	);
+
+	register_post_type( 'specialties', $args );
+}
+
+add_action( 'init', 'restro_specialties' );
